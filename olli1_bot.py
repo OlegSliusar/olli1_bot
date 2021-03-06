5cebc1f55d8a6fb363710378d80edd53510599b7
koch <koch@kali.opname>
2021-03-06 08:23:18 -0500

    Recieve functions

--- olli1_bot.py
+++ olli1_bot.py
@@ -40,6 +40,58 @@ def text(update, context):
         return received_birth_day(update, context)
 
 
+def received_birth_year(update, context):
+    global STATE
+
+    try:
+        today = datetime.date.today()
+        year = int(update.message.text)
+
+        if year > today.year:
+            raise ValueError("invalid value")
+
+        context.user_data['birth_year'] = year
+        update.message.reply_text(f"ok, now I need to know the month (in numerical form)...")
+        STATE = BIRTH_MONTH
+    except:
+        update.message.reply_text("it's funny but it doesn't seem to be correct...")
+
+def received_birth_month(update, context):
+    global STATE
+
+    try:
+        today = datetime.date.today()
+        month = int(update.message.text)
+
+        if month > 12 or month < 1:
+            raise ValueError("invalid value")
+
+        context.user_data['birth_month'] = month
+        update.message.reply_text(f"great! And now, the day...")
+        STATE = BIRTH_DAY
+    except:
+        update.message.reply_text("it's funny but it doesn't seem to be correct...")
+
+def received_birth_day(update, context):
+    global STATE
+
+    try:
+        today = datetime.date.today()
+        dd = int(update.message.text)
+        yyyy = context.user_data['birth_year']
+        mm = context.user_data['birth_month']
+        birthday = datetime.date(year=yyyy, month=mm, day=dd)
+
+        if today - birthday < datetime.timedelta(days=0):
+            raise ValueError("invalid value")
+
+        context.user_data['birthday'] = birthday
+        STATE = None
+        update.message.reply_text(f'ok, you born on {birthday}')
+
+    except:
+        update.message.reply_text("it's funny but it doesn't seem to be correct...")
+
 def main():
     TOKEN = "1651091717:AAGIb112p3wgd-ai2tZ4nO1t0QLp4nVRo6M"
 
@@ -55,8 +107,11 @@ def main():
     # add an handler for normal text (not commands)
     dispatcher.add_handler(MessageHandler(Filters.text, text))
 
+    # dispatcher.add_handler(CommandHandler("biorhythm", biorhythm))
+
     # add an handler for errors
     dispatcher.add_error_handler(error)
+
 
     # start your shiny new bot
     updater.start_polling()
